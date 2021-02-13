9715ceb20780b444b647712b46d6160849ba32fc
Tim James <32233010+tjames222@users.noreply.github.com>
2021-02-12 23:28:35 -0800

    Activity 4

---
+++ ACTIVITY 4/Activity1Part3/Activity1Part3/Services/Utilities/MyLogger1.cs
@@ -0,0 +1,45 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Activity1Part3.Services.Utilities
+{
+    public sealed class MyLogger1 : ILogger
+    {
+        private static MyLogger1 instance = new MyLogger1();
+        private static Logger getLogger = LogManager.GetCurrentClassLogger();
+        static MyLogger1()
+        {
+        }
+        private MyLogger1()
+        {
+        }
+        public static MyLogger1 Instance(string rules)
+        {
+            instance.Info(rules);
+            return instance;   
+        }
+        public static Logger GetLogger 
+        {
+            get
+            {
+                return getLogger;
+            }
+        }
+        public void Debug(string message)
+        {           
+        }
+        public void Error(string message)
+        {         
+        }
+        public void Info(string message)
+        {
+            getLogger.Info(message);
+        }
+        public void Warning(string message)
+        {  
+        }
+    }
+}
